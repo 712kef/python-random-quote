c98f027799693585b0dad3c26f84a3b5fca11e69
712kef <87010561+712kef@users.noreply.github.com>
2021-08-12 11:10:47 +0300

    Update get-quote.py

--- get-quote.py
+++ get-quote.py
@@ -2,7 +2,8 @@ def primary():
   f = open("quotes.txt")
   quotes = f.readlines()
   f.close() #comment
-  print(quotes) Anything added dilutes everything else
+  print(quotes[0]) Anything added dilutes everything else
+  
   if __name__== "__primary__":
   primary()
 # python get-quote.py
